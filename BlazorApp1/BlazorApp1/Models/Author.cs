4f7227287c1e47c0572f43df4ada2c2941796055
anisiceiu <anisictiu@gmail.com>
2024-02-07 18:19:48 +0600

    works update

--- BlazorApp1/BlazorApp1/Models/Author.cs
+++ BlazorApp1/BlazorApp1/Models/Author.cs
@@ -6,6 +6,7 @@ namespace BlazorApp1.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
